116b33cd621380da676564b814bd000c304e4d5e
Anup Sahu <anupsahu@cinqueon.com>
2018-09-27 02:13:26 +0530

    Initial Setup and added responsivenes to table

---
+++ client/app/main/main.component.js
@@ -0,0 +1,16 @@
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+import routing from './main.routes';
+
+export class MainController {
+
+
+}
+
+export default angular.module('daimondSweeperApp.main', [uiRouter])
+    .config(routing)
+    .component('main', {
+        template: require('./main.html'),
+        controller: MainController
+    })
+    .name;
