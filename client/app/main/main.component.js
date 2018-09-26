e89243bf55e1a856c85d44e215219c9838c824ef
Anup Sahu <anupsahu@cinqueon.com>
2018-09-27 04:35:58 +0530

    Basic working model
    - Added Random Daimonds generation
    
    - Check and validate cells

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -3,14 +3,64 @@ import uiRouter from 'angular-ui-router';
 import routing from './main.routes';
 
 export class MainController {
+    rows;
+    columns;
+    cell;
+    daimonds;
+    myClass = [];
+    /*@ngInject*/
+    constructor() {
+        this.rows = 8;
+        this.columns = 8;
+        this.cell = 1;
+
+        this.init();
+    }
 
 
+    //Random number generator
+    randomGenerator = (count) => {
+        let arr = [];
+        let min = 1;
+        let max = this.rows * this.columns;
+        while (arr.length < count) {
+            var randomnumber = Math.floor(Math.random() * (max - min + 1)) + min;
+            if (arr.indexOf(randomnumber) > -1) continue;
+            arr[arr.length] = randomnumber;
+        }
+        return arr;
+    }
+
+
+    init = () => {
+        this.daimonds = this.randomGenerator(5);
+        console.log(this.daimonds);
+        for (var i = 0; i < (this.rows * this.columns); i++) {
+            this.myClass[i] = 'unknown';
+        }
+    }
+
+    //Used to iterate ng-repeat as it only works
+    //on array instead of an integer
+    range = (num) => new Array(num);
+
+    //Counter to know track clicks
+    counter = () => this.cell++;
+
+    check = (cell) => {
+        console.log(cell);
+        if (_.includes(this.daimonds, cell))
+            this.myClass[cell - 1] = 'diamond';
+        else
+            this.myClass[cell - 1] = '';
+    }
 }
 
 export default angular.module('daimondSweeperApp.main', [uiRouter])
     .config(routing)
     .component('main', {
         template: require('./main.html'),
-        controller: MainController
+        controller: MainController,
+        controllerAs: 'vm'
     })
     .name;
